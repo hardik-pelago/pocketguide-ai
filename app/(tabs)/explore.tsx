ce464a12b5e06759d45f15314739f50231cb03ad
hardikamber <hardik.g@amberstudent.com>
2025-12-11 08:21:29 +0530

    Initial commit

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,15 +1,29 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Button, Platform, StyleSheet } from 'react-native';
+import { LLAMA3_2_1B_SPINQUANT, Message, useLLM } from 'react-native-executorch';
 
-import { Collapsible } from '@/components/ui/collapsible';
 import { ExternalLink } from '@/components/external-link';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
+import { Collapsible } from '@/components/ui/collapsible';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Fonts } from '@/constants/theme';
 
 export default function TabTwoScreen() {
+  const llm = useLLM({ model: LLAMA3_2_1B_SPINQUANT });
+  const handleGenerate = async() => {
+  const chat: Message[] = [
+    { role: 'system', content: 'You are a helpful assistant' },
+    { role: 'user', content: 'Hi!' },
+    { role: 'assistant', content: 'Hi!, how can I help you?' },
+    { role: 'user', content: 'What is the meaning of life?' },
+  ];
+
+  // Chat completion
+  const chatData= await llm.generate(chat);
+  console.log('chatData',chatData);
+};
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
@@ -21,6 +35,16 @@ export default function TabTwoScreen() {
           style={styles.headerImage}
         />
       }>
+        <ThemedView>
+                  <Button onPress={handleGenerate} title="Generate!" />
+         <ThemedText
+          type="title"
+          style={{
+            fontFamily: Fonts.rounded,
+          }}>
+          {llm.response}
+        </ThemedText>
+        </ThemedView>
       <ThemedView style={styles.titleContainer}>
         <ThemedText
           type="title"
